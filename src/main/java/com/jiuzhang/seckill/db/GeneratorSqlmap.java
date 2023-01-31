d76d2994b69a01a4e7f4562f6225550fcd76ef1e
Shurui Liu <shurui91@gmail.com>
2023-01-30 17:31:11 -0800

    DaoTest is done

---
+++ src/main/java/com/jiuzhang/seckill/db/GeneratorSqlmap.java
@@ -0,0 +1,31 @@
+package com.jiuzhang.seckill.db;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.*;
+
+public class GeneratorSqlmap {
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generator() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        //指定逆向工程配置文件
+        File configFile = new File("src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}

ce4e411d11c9b99fb7d8fbe1227edba853d3d4c0
Shurui Liu <shurui91@gmail.com>
2023-02-01 10:20:41 -0800

    working on 3.5.3

--- src/main/java/com/jiuzhang/seckill/web/SeckillActivityController.java
+++ src/main/java/com/jiuzhang/seckill/web/SeckillActivityController.java
@@ -1,21 +1,27 @@
 package com.jiuzhang.seckill.web;
 
 import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
+import com.jiuzhang.seckill.db.dao.SeckillCommodityDao;
 import com.jiuzhang.seckill.db.po.SeckillActivity;
+import com.jiuzhang.seckill.db.po.SeckillCommodity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class SeckillActivityController {
     @Autowired
     private SeckillActivityDao seckillActivityDao;
+    @Autowired
+    private SeckillCommodityDao seckillCommodityDao;
 
     @RequestMapping("/addSeckillActivity")
     public String addSeckillActivity() {
@@ -53,12 +59,38 @@ public class SeckillActivityController {
         return "add_success";
     }
 
-    // 展示已经加入的秒杀活动
+    /**
+     * 查询秒杀活动的列表
+     *
+     * @param resultMap
+     * @return
+     */
     @RequestMapping("/seckills")
     public String activityList(HashMap<String, Object> resultMap) {
         List<SeckillActivity> seckillActivities =
                 seckillActivityDao.querySeckillActivitysByStatus(1);
         resultMap.put("seckillActivities", seckillActivities);
         return "seckill_activity";
+    }
+
+    /**
+     * 秒杀商品详情
+     *
+     * @param resultMap
+     * @param seckillActivityId
+     * @return
+     */
+    @RequestMapping("/item/{seckillActivityId}")
+    public String itemPage(HashMap<String, Object> resultMap, @PathVariable long seckillActivityId) {
+        SeckillActivity seckillActivity = seckillActivityDao.querySeckillActivityById(seckillActivityId);
+        SeckillCommodity seckillCommodity = seckillCommodityDao.querySeckillCommodityById(seckillActivity.getCommodityId());
+        resultMap.put("seckillActivity", seckillActivity);
+        resultMap.put("seckillCommodity", seckillCommodity);
+        resultMap.put("seckillPrice", seckillActivity.getSeckillPrice());
+        resultMap.put("oldPrice", seckillActivity.getOldPrice());
+        resultMap.put("commodityId", seckillActivity.getCommodityId());
+        resultMap.put("commodityName", seckillCommodity.getCommodityName());
+        resultMap.put("commodityDesc", seckillCommodity.getCommodityDesc());
+        return "seckill_item";
     }
 }

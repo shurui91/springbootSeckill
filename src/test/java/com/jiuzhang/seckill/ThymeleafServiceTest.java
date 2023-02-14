be87ec1e23acf5b69c957d7a0fac5937dc2e52a8
Shurui Liu <shurui91@gmail.com>
2023-02-14 13:19:30 -0800

    static webpages can be created by Thymeleaf now

---
+++ src/test/java/com/jiuzhang/seckill/ThymeleafServiceTest.java
@@ -0,0 +1,18 @@
+package com.jiuzhang.seckill;
+
+import com.jiuzhang.seckill.web.ActivityHtmlPageService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ThymeleafServiceTest {
+
+    @Autowired
+    private ActivityHtmlPageService activityHtmlPageService;
+
+    @Test
+    public void createHtmlTest() {
+        activityHtmlPageService.createActivityHtml(19);
+    }
+}
